786e98145a4a33a28166a02809078e5ef4a56beb
Artem Papeta <ar.papeta@gmail.com>
2020-08-25 22:19:25 +0300

    init web service

---
+++ AccessControlWebApp/Models/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessControlWebApp.Models
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
